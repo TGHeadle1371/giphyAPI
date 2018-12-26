50d9ae6f7c7026618bb3d20bf7e4cf146d7f58a7
TGHeadle1371 <tgheadle@gmail.com>
2018-12-26 13:32:58 -0700

    gif download API, not working

--- assets/javascript/giphyAPI.js
+++ assets/javascript/giphyAPI.js
@@ -56,13 +56,13 @@ $(document).ready(function () {
     ];
     //Functions
     //Create variable that is a function
-    var addBtn = function (topic) {
+    var addBtn = function (search) {
         // Define searchBTN as a button element with set class and attr
-        var searchBtn = $('<button>').addClass("topic btn btn-primary").attr("data-topic", topic).text(topic);
+        var searchBtn = $('<button>').addClass("search btn btn-primary").attr("data-search", search).text(search);
         // on button click, run function
         searchBtn.on("click", function () {
             // Build api search by defining term, api key, and chaining URL
-            var term = $(this).attr("data-topic");
+            var term = $(this).attr("data-search");
             var apiKey = "WSY305l72MwB3JpnUG74hV5Fp85piPgx";
             //Api search + term + limit of 10 + api key
             var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + term + "&limit=10&api_key=" + apiKey;
@@ -99,26 +99,27 @@ $(document).ready(function () {
                     // Create gifs and set attr
                     var gif = $("<img>").addClass("gif");
                     gif.attr("src", data[i].images.fixed_height_still.url);
-                    gif.attr("data-state", "still");
+                    gif.attr("data-gifImg", "still");
                     gif.attr("data-still", data[i].images.fixed_height_still.url);
                     gif.attr("data-animate", data[i].images.fixed_height.url);
 
                     //Create gif on click play 
-                    gif.on("click", function () {
+                    gif.on("click", function() {
                         //Create state variable for the playing state of the gif
-                        var state = $(this).attr("data-state");
+                        var gifImg = $(this).attr("data-gifImg");
 
-                        if (state === "still") {
+                        if (gifImg === "still") {
                             //If still, animate
-                            $(this).attr("data-state", "animate");
+                            $(this).attr("data-gifImg", "animate");
                             $(this).attr("src", $(this).attr("data-animate"));
-                        } else if (state === "animate") {
+                        } else if (gifImg === "animate") {
                             //Make gif still
-                            $(this).attr("data-state", "still");
+                            $(this).attr("data-gifImg", "still");
                             $(this).attr("src", $(this).attr("data-still"));
                         }
 
                     });
+                    
 
                     //Append title rating and gif 
                     title.appendTo(gifDiv);
@@ -142,6 +143,7 @@ $(document).ready(function () {
     };
 
     //Event listener
+    //Add gif On click function
     $("#add").on("click", function (event) {
         //No refresh
         event.preventDefault();
@@ -152,6 +154,11 @@ $(document).ready(function () {
             addBtn(term);
         }
     });
+    // //Download on click
+    // $('#download').click(function (e) {
+    //     e.preventDefault();  //stop the browser from following
+    //     window.location.href = search;
+    // });
     //Add the terms
     addAllTerms();
 });
